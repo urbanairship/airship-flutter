038f8bb2975440e6d79acbdeafe23b46665d1702
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2019-06-04 13:11:39 +0200

    Fix build error

--- ios/Classes/Events/AirshipChannelUpdatedEvent.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class AirshipChannelUpdatedEvent : AirshipChannelCreatedEvent {
-    override var eventType: AirshipEventType {
-        get {
-            return AirshipEventType.ChannelUpdated
-        }
-    }
-}
