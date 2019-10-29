005c836efff5a075407ba1714bf74422b91780d4
David <jdavidcrow@gmail.com>
2019-10-29 13:59:48 -0700

    [MOBILE-944] update sample to use inbox message view callbacks

--- example/lib/screens/message_view.dart
+++ example/lib/screens/message_view.dart
@@ -2,33 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:airship/airship.dart';
 import 'package:airship_example/styles.dart';
 
-class MessageView extends StatelessWidget {
+class MessageView extends StatefulWidget {
   final InboxMessage message;
 
   MessageView({this.message});
 
   @override
+  _MessageViewState createState() => _MessageViewState();
+}
+
+class _MessageViewState extends State<MessageView>  {
+  bool isLoading = true;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("${message.title}"),
+          title: isLoading ?  Text("Loading...") : Text("${widget.message.title}"),
+
           backgroundColor: Styles.background,
         ),
-        body: Container(
-          child: InboxMessageView(onViewCreated:onInboxMessageViewCreated),
-        )
+        body: Stack(
+              children: <Widget>[isLoading ? Center(child:CircularProgressIndicator()) : Container(), InboxMessageView(message: widget.message,
+                onLoadStarted: onStarted,
+                onLoadFinished: onLoadFinished)
+        ]),
     );
   }
 
-  void onInboxMessageViewCreated(InboxMessageViewController controller) {
-    Airship.inboxMessages.then((List<InboxMessage> messages) {
-      if (message != null) {
-        debugPrint("Loading message: ${message.title}");
-        controller.loadMessage(message);
-        Airship.markInboxMessageRead(message);
-      } else {
-        debugPrint("Attempted to load message view for null message");
-      }
+  onStarted() {
+    setState(() {
+      isLoading = true;
+    });
+  }
+
+  onLoadFinished() {
+    setState(() {
+      isLoading = false;
     });
   }
 }
