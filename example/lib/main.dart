16d931262425de07be8a1fe5393ffb93998b6d26
Brian Batchelder <brian@briansbrain.biz>
2020-08-04 14:23:08 -0700

    Fix exception in example app.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,6 +16,7 @@ const String message_center_deep_link =  "message_center";
 const String settings_deep_link =  "settings";
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
