5e5cdd11db2e665676c15eda56d48cb47e0e2d0c
Ulrich Giberné <ulrich.giberne@airship.com>
2021-03-24 11:18:11 -0400

    small fix

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -29,9 +29,8 @@ done
 
 # Generate doc
 if $GENERATE; then
-  # TODO Check if one if this have been removed in flutter 2 (changelog)
   # TODO Then check if something has to be activated for dartdoc to work in the CI
-  $FLUTTER_ROOT/bin/cache/dart-sdk/bin/dartdoc #--exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:ffi,dart:html,dart:io,dart:isolate,dart:js,dart:js_util,dart:math,dart:typed_data,dart:ui'
+  $FLUTTER_ROOT/bin/cache/dart-sdk/bin/dartdoc
 fi
 
 # Upload doc
