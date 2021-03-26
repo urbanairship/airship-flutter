39859f4d2ab000a2d56e965b5e13f841e233cfa3
Ulrich Giberné <ulrich.giberne@airship.com>
2021-03-26 15:02:51 -0400

    test

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -32,7 +32,7 @@ if $GENERATE; then
   $FLUTTER_ROOT/bin/cache/dart-sdk/bin/dart --version
   $FLUTTER_ROOT/bin/cache/dart-sdk/bin/dartdoc --version
   $FLUTTER_ROOT/bin/flutter --version
-  $FLUTTER_ROOT/bin/cache/dart-sdk/bin/dartdoc --exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:ffi,dart:html,dart:io,dart:isolate,dart:js,dart:js_util,dart:math,dart:typed_data,dart:ui'
+  $FLUTTER_ROOT/bin/cache/dart-sdk/bin/dartdoc --exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:ffi,dart:html,dart:io,dart:isolate,dart:js,dart:js_util,dart:math,dart:typed_data,dart:ui,flutter'
 fi
 
 # Upload doc
