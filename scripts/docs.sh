7ea04ec87785cfbe2c8d49a36c2d24ae9b689a05
Ulrich Giberne <ulrich.giberne@airship.com>
2021-09-27 11:59:47 -0400

    test

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -31,6 +31,7 @@ done
 if $GENERATE; then
   flutter pub get
   flutter pub global activate dartdoc
+  FLUTTER_ROOT/bin/cache/dart-sdk/bin/dart --version
   flutter pub global run dartdoc --exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:ffi,dart:html,dart:io,dart:isolate,dart:js,dart:js_util,dart:math,dart:typed_data,dart:ui'
 fi
 
