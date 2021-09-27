28404637b863fbad31cd7c84a44ef81185f5fd0e
Ulrich Giberne <ulrich.giberne@airship.com>
2021-09-27 12:20:44 -0400

    test

--- scripts/run_ci_tasks.sh
+++ scripts/run_ci_tasks.sh
@@ -40,7 +40,8 @@ flutter packages get
 # Flutter Analysis
 if $ANALYZE; then
     flutter analyze
-    FLUTTER_ROOT/bin/cache/dart-sdk/bin/dart --version
+    $FLUTTER_ROOT/bin/flutter --version
+    $FLUTTER_ROOT/bin/cache/dart-sdk/bin/dart --version
     # Perform publish dry run to ensure the package can be published
     flutter pub pub publish --dry-run
 fi
