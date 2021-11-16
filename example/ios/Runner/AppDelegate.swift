8cb89e3485cd1243dc91a2f749e7049db9cea9fc
Ognjen Ristanovic <ognjen.ristanovic@airship.com>
2021-11-16 17:04:20 +0100

    move take off to plugin

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -8,7 +8,6 @@ import AirshipKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    Airship.takeOff(launchOptions: launchOptions)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
