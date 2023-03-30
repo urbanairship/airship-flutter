02c3164955516b92b7511ef38e3417b4599bf9cd
Ognjen Ristanović <ognjen.ristanovic@urbanairship.com>
2023-03-30 10:14:48 +0200

    Release 6.3.0 (#165)
    * prepare release
    
    * compile version
    
    ---------
    
    Co-authored-by: Ognjen Ristanovic <ognjen.ristanovic@airship.com>

--- android/src/main/kotlin/com/airship/flutter/AirshipPluginVersion.kt
+++ android/src/main/kotlin/com/airship/flutter/AirshipPluginVersion.kt
@@ -2,6 +2,6 @@ package com.airship.flutter
 
 class AirshipPluginVersion {
     companion object {
-        const val AIRSHIP_PLUGIN_VERSION = "6.2.3"
+        const val AIRSHIP_PLUGIN_VERSION = "6.3.0"
     }
 }
