d840810e7d76ea89b16f66c20c8bce7753200df3
Arunshaik2001 <arunshaik432@gmail.com>
2023-05-13 12:27:38 +0530

    Moved method channel method names to constants.dart for better readability and make less human error while typing method names

---
+++ lib/src/constants.dart
@@ -0,0 +1,40 @@
+
+
+class MethodNameConstants {
+  static const backgroundIsolateStarted = "backgroundIsolateStarted";
+  static const takeOff = "takeOff";
+  static const getChannelId = "getChannelId";
+  static const setUserNotificationsEnabled = "setUserNotificationsEnabled";
+  static const clearNotification = "clearNotification";
+  static const clearNotifications = "clearNotifications";
+  static const getTags = "getTags";
+  static const getInboxMessages = "getInboxMessages";
+  static const addEvent = "addEvent";
+  static const addTags = "addTags";
+  static const removeTags = "removeTags";
+  static const editAttributes = "editAttributes";
+  static const setNamedUser = "setNamedUser";
+  static const markInboxMessageRead = "markInboxMessageRead";
+  static const deleteInboxMessage = "deleteInboxMessage";
+  static const refreshInbox = "refreshInbox";
+  static const getNamedUser = "getNamedUser";
+  static const getUserNotificationsEnabled = "getUserNotificationsEnabled";
+  static const getActiveNotifications = "getActiveNotifications";
+  static const enableChannelCreation = "enableChannelCreation";
+  static const setInAppAutomationPaused = "setInAppAutomationPaused";
+  static const getInAppAutomationPaused = "getInAppAutomationPaused";
+  static const trackScreen = "trackScreen";
+  static const isAutoBadgeEnabled = "isAutoBadgeEnabled";
+  static const setAutoBadgeEnabled = "setAutoBadgeEnabled";
+  static const setBadge = "setBadge";
+  static const resetBadge = "resetBadge";
+  static const enableFeatures = "enableFeatures";
+  static const disableFeatures = "disableFeatures";
+  static const setEnabledFeatures = "setEnabledFeatures";
+  static const getEnabledFeatures = "getEnabledFeatures";
+  static const isFeatureEnabled = "isFeatureEnabled";
+  static const openPreferenceCenter = "openPreferenceCenter";
+  static const getSubscriptionLists = "getSubscriptionLists";
+  static const getPreferenceCenterConfig = "getPreferenceCenterConfig";
+  static const setAutoLaunchDefaultPreferenceCenter = "setAutoLaunchDefaultPreferenceCenter";
+}
