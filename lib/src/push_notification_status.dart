9d019f0a85d2ea2ddceda5c5388875d716a08c7e
Apekka <antoine.pery-kasza@airship.com>
2023-07-10 17:57:20 +0200

    add missing file

---
+++ lib/src/push_notification_status.dart
@@ -0,0 +1,48 @@
+
+/// Push notification status object.
+class PushNotificationStatus {
+
+  /// If user notifications are enabled.
+  final bool? isUserNotificationsEnabled;
+
+  /// If notifications are allowed at the system level for the application.
+  final bool? areNotificationsAllowed;
+
+  /// If the push feature is enabled on PrivacyManager.
+  final bool? isPushPrivacyFeatureEnabled;
+
+  /// If push registration was able to generate a token.
+  final bool? isPushTokenRegistered;
+
+  /// If Airship is able to send and display a push notification.
+  final bool? isOptedIn;
+
+  /**
+  * Checks for isUserNotificationsEnabled, areNotificationsAllowed, and isPushPrivacyFeatureEnabled. If this flag
+  * is true but `isOptedIn` is false, that means push token was not able to be registered.
+  **/
+  final bool? isUserOptedIn;
+
+  const PushNotificationStatus._internal(
+      this.isUserNotificationsEnabled, this.areNotificationsAllowed, this.isPushPrivacyFeatureEnabled,
+      this.isPushTokenRegistered, this.isOptedIn, this.isUserOptedIn);
+
+  static PushNotificationStatus fromJson(Map<String, dynamic> json) {
+    var isUserNotificationsEnabled = json["isUserNotificationsEnabled"];
+    var areNotificationsAllowed = json["areNotificationsAllowed"];
+    var isPushPrivacyFeatureEnabled = json["isPushPrivacyFeatureEnabled"];
+    var isPushTokenRegistered = json["isPushTokenRegistered"];
+    var isOptedIn = json["isOptedIn"];
+    var isUserOptedIn = json["isUserOptedIn"];
+    return PushNotificationStatus._internal(
+        isUserNotificationsEnabled, areNotificationsAllowed, isPushPrivacyFeatureEnabled,
+        isPushTokenRegistered, isOptedIn, isUserOptedIn);
+  }
+
+  @override
+  String toString() {
+    return "PushNotificationStatus(isUserNotificationsEnabled=$isUserNotificationsEnabled,"
+        " areNotificationsAllowed=$areNotificationsAllowed, isPushPrivacyFeatureEnabled=$isPushPrivacyFeatureEnabled,"
+        " isPushTokenRegistered=$isPushTokenRegistered, isOptedIn=$isOptedIn, isUserOptedIn=$isUserOptedIn)";
+  }
+}
