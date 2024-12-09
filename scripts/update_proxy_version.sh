ea7d78515c8becb8f9d53bb012603882a7d8e687
crow <david.crow@airship.com>
2024-12-08 17:13:04 -0800

    Fix update_proxy_version script

--- scripts/update_proxy_version.sh
+++ scripts/update_proxy_version.sh
@@ -1,23 +1,16 @@
-#!/bin/bash
-set -e
-set -x
+#!/usr/bin/env bash
+set -euxo pipefail
 
-SCRIPT_DIRECTORY="$(dirname "$0")"
-ROOT_PATH=$SCRIPT_DIRECTORY/../
+SCRIPT_DIRECTORY="$(cd "$(dirname "$0")" && pwd)"
+ROOT_PATH="$SCRIPT_DIRECTORY/.."
 
-PROXY_VERSION=$1
-
-if [ -z "$1" ]
-  then
+PROXY_VERSION="$1"
+if [ -z "$PROXY_VERSION" ]; then
     echo "No proxy version supplied"
     exit 1
 fi
 
-# Update Android gradle.properties
-sed -i '' "s/\(ext.airship_framework_proxy_version *= *\)\".*\"/\1\"$PROXY_VERSION\"/" "$ROOT_PATH/android/build.gradle"
+sed -i.bak -E "s/(ext\.airship_framework_proxy_version *= *')([^']*)(')/\1$PROXY_VERSION\3/" "$ROOT_PATH/android/build.gradle"
+sed -i.bak -E "s/(s\.dependency *\"AirshipFrameworkProxy\", *\")([^\"]*)(\")/\1$PROXY_VERSION\3/" "$ROOT_PATH/ios/airship_flutter.podspec"
 
-# Update iOS podspec
-sed -i '' "s/\(pod.*AirshipFrameworkProxy.*\)'\([^']*\)'/\1'$PROXY_VERSION'/" "$ROOT_PATH/ios/airship_flutter.podspec"
-
-# Update Package.swift
-sed -i '' "s/\(url: \"https:\/\/github.com\/urbanairship\/airship-mobile-framework-proxy.git\", from: \)\"[^\"]*\"/\1\"$PROXY_VERSION\"/" "$ROOT_PATH/Package.swift"
+find "$ROOT_PATH" -name "*.bak" -delete
