93ee946c6501f74e7022a916a6085c267b080b1b
infra-source <infra-source@users.noreply.github.com>
2025-01-02 21:37:34 +0000

    Release 11.2.2

--- android/src/main/kotlin/com/airship/flutter/AirshipPluginVersion.kt
+++ android/src/main/kotlin/com/airship/flutter/AirshipPluginVersion.kt
@@ -2,6 +2,6 @@ package com.airship.flutter
 
 class AirshipPluginVersion {
     companion object {
-        const val AIRSHIP_PLUGIN_VERSION = "9.1.0"
+        const val AIRSHIP_PLUGIN_VERSION = "11.2.2"
     }
 }
