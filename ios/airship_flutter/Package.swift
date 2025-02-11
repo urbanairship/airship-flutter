c43138a2c9953e9fae850ffe7b6c0f1858382992
David <jdavidcrow@gmail.com>
2025-02-11 11:43:47 -0800

    update proxy script with SPM support and run it

--- ios/airship_flutter/Package.swift
+++ ios/airship_flutter/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library( name: "airship-flutter", targets: ["airship_flutter"])
     ],
     dependencies: [
-        .package(url: "https://github.com/urbanairship/airship-mobile-framework-proxy.git", from: "12.1.0")
+        .package(url: "https://github.com/urbanairship/airship-mobile-framework-proxy.git", from: "13.0.0")
     ],
     targets: [
         .target(
