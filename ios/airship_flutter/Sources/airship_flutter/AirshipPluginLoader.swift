370b62fa18e376aa57c2ff9a82f9f00b1f6a8007
Ryan Lepinski <ryan.lepinski@airship.com>
2025-08-27 16:58:06 -0700

    [MOBILE-5230] Add check for flutter state before emitting events (#268)
    * [MOBILE-5230] Add check for flutter state before emitting events
    
    * Fixes
    
    * Fix
    
    * Fix feature flag methods
    
    * Release 10.8.0

--- ios/airship_flutter/Sources/airship_flutter/AirshipPluginLoader.swift
+++ ios/airship_flutter/Sources/airship_flutter/AirshipPluginLoader.swift
@@ -6,6 +6,6 @@ public class AirshipPluginLoader: NSObject, AirshipPluginLoaderProtocol {
     public static func onApplicationDidFinishLaunching(
         launchOptions: [UIApplication.LaunchOptionsKey : Any]?
     ) {
-        AirshipAutopilot.shared.onLoad(launchOptions: launchOptions)
+        AirshipAutopilot.shared.onLoad()
     }
 }
